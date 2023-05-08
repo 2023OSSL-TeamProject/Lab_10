f2842b548000b29eeb1b031e3e79f9d0bf6d38fb
김동규 <kdg1941@naver.com>
2023-05-08 21:54:18 +0900

    update user_CRUD.c / user_CURD.h

--- src/inc/user_CRUD.c
+++ src/inc/user_CRUD.c
@@ -1,1 +1,3 @@
 // user에 대한 CRUD를 기능을 구현하는 파일입니다.
+#include <stdio.h>
+#include "user_CRUD.h"
--- src/inc/user_CRUD.h
+++ src/inc/user_CRUD.h
@@ -2,6 +2,7 @@
 
 typedef struct _user {
     int studentNum; // user 학번
+    int money; // user가 가지고있는 돈
     char name[20]; // user 이름
     char washer[20]; // 사용중인 세탁기 이름,,?
 } User;
@@ -10,3 +11,8 @@ typedef struct _list {
     User user; // 노드 안에서 user의 데이터를 저장
     List *link; // list를 가리키는 링크
 } List;
+
+void Insert(List *list); // list에서 노드 추가
+void Delete(List *list); // list에서 노드 삭제
+void Print(List *list); // list print
+void IsEmpty(List *list); // list가 비어있는지 확인
