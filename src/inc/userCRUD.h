f2a3ceda1dab3148fc153035b60c141517b8a905
pangil5634 <oksk6681@naver.com>
2023-05-15 21:36:58 +0900

    KKI : update userCRUD

--- src/inc/userCRUD.h
+++ src/inc/userCRUD.h
@@ -2,13 +2,12 @@
 
 typedef struct _user
 {
-    int studentNum;  // user 학번
-    int money;       // user가 가지고있는 돈
-    char name[20];   // user 이름
-    char washer[20]; // 사용중인 세탁기 이름,,?
-    int detergent; // 세제 수량
+    int studentNum;        // user 학번
+    int money;             // user가 가지고있는 돈
+    char name[20];         // user 이름
+    char washer[20];       // 사용중인 세탁기 이름,,?
+    int detergent;         // 세제 수량
     int fabricConditioner; // 섬유유연제 수량
-
 
 } User;
 
@@ -18,10 +17,10 @@ typedef struct _list
     struct _list *link; // list를 가리키는 링크
 } List;
 
-void ListInit(List *list);         // list의 초기화를 담당한다
-int ListInsert(List *list);        // list에서 노드 추가
-int ListDelete(List *list);        // list에서 노드 삭제
-void ListPrint(List *list);        // list print
-void ListIsEmpty(List *list);      // list가 비어있는지 확인
-void ListUpdate(List *list);       // user에 대한 정보 수정
-struct _list* ListFind(List *list, int num); // 원하는 user 탐색
+void ListInit(List *list);                   // list의 초기화를 담당한다
+int ListInsert(List *list);                  // list에서 노드 추가
+int ListDelete(List *list);                  // list에서 노드 삭제
+void ListPrint(List *list);                  // list print
+void ListIsEmpty(List *list);                // list가 비어있는지 확인
+int ListUpdate(List *list);                  // user에 대한 정보 수정
+struct _list *ListFind(List *list, int num); // 원하는 user 탐색
