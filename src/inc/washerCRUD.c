ffb0b0362a5374000fb53ab308d0ca911c49a5b3
pangil5634 <oksk6681@naver.com>
2023-05-16 23:05:12 +0000

    KKI : Create ChoiceWasher Function2

--- src/inc/washerCRUD.c
+++ src/inc/washerCRUD.c
@@ -1,30 +1,91 @@
 // 세탁기에 대한 CRUD
 #include <stdio.h>
 #include "washerCRUD.h"
+#include "otherFunc.h"
 
 typedef struct _washer
 {
-    int studentNum; // user 학번
-    char name[20];  // user 이름
+    char UserName[20]; // User 이름
+    int UserNumber;    // User 학번
+    int UserTime;      // 세탁기 사용 시간
 
 } Washer;
 
+char dormitoryInfo[2][20] = {"비전관", "벧엘관"};
+char floorInfo[4][20] = {"1층", "2층", "3층", "4층"};
+char washerInfo[2][20] = {"세탁기1", "세탁기2"};
+// 세탁기 선택 함수
 int ChoiceWaseher()
 {
-    int dormitory = 0;
-    int floor = 0;
-    int washer = 0;
+    int dormitory = 0; // 기숙사
+    int floor = 0;     // 층
+    int washer = 0;    // 세탁기
 
-    printf("(1) 비전관 | (2) 벧엘관\n");
+    // 사용자로부터 기숙사, 층, 세탁기 정보 입력 받기 (예외처리까지)
+
+    // 초기값 세팅
+    dormitory = -1;
+    floor = -1;
+    washer = -1;
+
+    printf("(1) 비전관 | (2) 벧엘관 | (0) 취소\n");
     printf("=> 기숙사를 선택하세요 : ");
     scanf("%d", &dormitory);
-    printf("(1) 1층 | (2) 2층 | (3) 3층 | (4) 4층\n");
+    println();
+    if (dormitory == 0)
+    {
+        printf("종료되었습니다.\n");
+        println();
+        return 0;
+    }
+    else if (dormitory < 1 || dormitory > 2)
+    {
+        printf("입력하신 기숙사는 없습니다.\n");
+        println();
+        return 0;
+    }
+
+    printf("(1) 1층 | (2) 2층 | (3) 3층 | (4) 4층 | (0) 취소\n");
     printf("=> 층을 선택하세요 : ");
     scanf("%d", &floor);
-    printf("(1) 세탁기1 | (2) 세탁기2\n");
-    printf("=> 세탁기를 선택하세요 : ");
-    printf("%d", &washer);
+    println();
+    if (floor == 0)
+    {
+        printf("종료되었습니다.\n");
+        println();
+        return 0;
+    }
+    else if (floor < 1 || floor > 4)
+    {
+        printf("입력하신 층은 없습니다.\n");
+        println();
+        return 0;
+    }
 
+    printf("(1) 세탁기1 | (2) 세탁기2 | (0) 취소\n");
+    printf("=> 세탁기를 선택하세요 : ");
+    scanf("%d", &washer);
+    println();
+    if (washer == 0)
+    {
+        printf("종료되었습니다.\n");
+        println();
+        return 0;
+    }
+    else if (washer < 1 || washer > 2)
+    {
+        printf("입력하신 세탁기는 없습니다.\n");
+        println();
+        return 0;
+    }
+
+    printf("세탁하신 세탁기 정보\n");
+    printf("기숙사 : %s\n", dormitoryInfo[dormitory - 1]);
+    printf("층 : %s\n", floorInfo[floor - 1]);
+    printf("세탁기 : %s\n", washerInfo[washer - 1]);
+    println();
+
+    // 기숙사, 층, 세탁기를 정상적으로 입력한 경우
     if (dormitory == 1)
     {
         if (floor == 1)
@@ -119,4 +180,5 @@ int ChoiceWaseher()
             }
         }
     }
-};
+    return 0;
+}
